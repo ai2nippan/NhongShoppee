245d7beaef6050b5bcdfe82c0f1f9482d1f596fe
test <test@abc.com>
2020-02-09 14:27:51 +0700

    Detail Success

---
+++ lib/screens/detail.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:nhongshoppee/models/product_model.dart';
+import 'package:nhongshoppee/utility/my_style.dart';
+
+class Detail extends StatefulWidget {
+  final ProductModel productModel;
+
+  Detail({Key key, this.productModel}) : super(key: key);
+
+  @override
+  _DetailState createState() => _DetailState();
+}
+
+class _DetailState extends State<Detail> {
+  // Field
+  ProductModel currentProductModel;
+
+  // Method
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    currentProductModel = widget.productModel;
+  }
+
+  Widget orderButton() {
+    return FloatingActionButton(
+      // backgroundColor: Colors.blue,
+      child: Icon(Icons.add),
+      onPressed: () {},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var text = Text('Price = ${currentProductModel.price}');
+    return Scaffold(
+      floatingActionButton: orderButton(),
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Text(
+              currentProductModel.nameFood,
+              style: MyStyle().headTextStyle,
+            ),
+            Text(
+              currentProductModel.detail,
+              style: MyStyle().bodyTextStyle,
+            ),
+            Text(
+              'Price = ${currentProductModel.price}',
+              style: MyStyle().headColorTextStyle,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/utility/my_style.dart
+++ lib/utility/my_style.dart
@@ -1,10 +1,17 @@
+import 'package:flutter/material.dart';
+
 class MyStyle {
-  
   String appName = 'Nhong ShoppEE';
 
+  TextStyle headTextStyle =
+      TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold);
 
+  TextStyle headColorTextStyle =
+      TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold,color: Colors.red.shade800);
 
+  TextStyle bodyTextStyle = TextStyle(
+    fontSize: 18.0,
+  );
 
   MyStyle();
-
 }
